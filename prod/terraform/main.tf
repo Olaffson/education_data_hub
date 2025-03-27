8487660366627c875b50c2bc5d26341ef7fd7df8
Olaffson <olivierkotwica@gmail.com>
2025-03-27 16:18:32 +0100

    Fix : bdd sql via terraform

--- prod/terraform/main.tf
+++ prod/terraform/main.tf
@@ -72,44 +72,44 @@ resource "azurerm_role_assignment" "adf_storage_contributor" {
   principal_id         = azurerm_data_factory.adf.identity[0].principal_id
 }
 
-# Serveur SQL Azure
-resource "azurerm_mssql_server" "sql_server" {
-  name                         = "sqlserver-${var.environment}"
-  resource_group_name          = azurerm_resource_group.rg.name
-  location                     = azurerm_resource_group.rg.location
-  version                      = "12.0"
-  administrator_login          = var.sql_admin_username
-  administrator_login_password = var.sql_admin_password
+# # Serveur SQL Azure
+# resource "azurerm_mssql_server" "sql_server" {
+#   name                         = "sqlserver-${var.environment}"
+#   resource_group_name          = azurerm_resource_group.rg.name
+#   location                     = azurerm_resource_group.rg.location
+#   version                      = "12.0"
+#   administrator_login          = var.sql_admin_username
+#   administrator_login_password = var.sql_admin_password
 
-  minimum_tls_version = "1.2"
+#   minimum_tls_version = "1.2"
 
-  tags = {
-    environment = var.environment
-  }
-}
+#   tags = {
+#     environment = var.environment
+#   }
+# }
 
-# Base de données Azure SQL
-resource "azurerm_mssql_database" "education_db" {
-  name                 = "EducationData"
-  server_id            = azurerm_mssql_server.sql_server.id
-  sku_name             = "Basic"
-  max_size_gb          = 2
-  zone_redundant       = false
-  collation            = "SQL_Latin1_General_CP1_CI_AS"
-  storage_account_type = "Local"
+# # Base de données Azure SQL
+# resource "azurerm_mssql_database" "education_db" {
+#   name                 = "EducationData"
+#   server_id            = azurerm_mssql_server.sql_server.id
+#   sku_name             = "Basic"
+#   max_size_gb          = 2
+#   zone_redundant       = false
+#   collation            = "SQL_Latin1_General_CP1_CI_AS"
+#   storage_account_type = "Local"
 
-  tags = {
-    environment = var.environment
-  }
-}
+#   tags = {
+#     environment = var.environment
+#   }
+# }
 
-# Pare-feu pour autoriser l'accès au SQL Server depuis tous les services Azure
-resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
-  name             = "AllowAzureServices"
-  server_id        = azurerm_mssql_server.sql_server.id
-  start_ip_address = "0.0.0.0"
-  end_ip_address   = "0.0.0.0"
-}
+# # Pare-feu pour autoriser l'accès au SQL Server depuis tous les services Azure
+# resource "azurerm_mssql_firewall_rule" "allow_azure_services" {
+#   name             = "AllowAzureServices"
+#   server_id        = azurerm_mssql_server.sql_server.id
+#   start_ip_address = "0.0.0.0"
+#   end_ip_address   = "0.0.0.0"
+# }
 
 # # Dataset source pour les fichiers CSV de data_gouv
 # resource "azurerm_data_factory_dataset_delimited_text" "data_gouv_csv" {
